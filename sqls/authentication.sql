bd79260dae5bf740916d7a8ef154c5b6b0cbfd6d
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-02-25 15:39:58 +0900

    Update authentication database

--- sqls/authentication.sql
+++ sqls/authentication.sql
@@ -11,37 +11,45 @@ create table groups
 );
 
 
+
 create table machines
 (
-    id              integer
+    id                integer
         constraint machines_pk
             primary key autoincrement,
-    type            text    not null,
-    group_full_path integer not null,
-    group_uid       integer not null,
-    name            text    not null,
-    owner           text    not null,
-    policies        text    not null,
-    totp            text    not null,
-    enc_publickey   text    not null
+    machine_type      text                    not null,
+    group_full_path   integer                 not null,
+    group_uid         integer                 not null,
+    name              text                    not null,
+    owner             text                    not null,
+    policies          text                    not null,
+    machine_totp      text                    not null,
+    server_pk         text                    not null,
+    uid               text                    not null,
+    network           text                    not null,
+    client_pk         text                    not null,
+    machine_full_name text                    not null,
+    auth_method       text default 'software' not null,
+    hardware_ak       text
 );
 
 create table users
 (
-    id                      integer
+    id              integer
         constraint int
             primary key autoincrement,
-    uid                     TEXT                not null,
-    email                   text                not null,
-    name                    text                not null,
-    profilepic              text,
-    max_devices             integer default 999 not null,
-    reset_password_on_logon INT,
-    password                text                not null,
-    password_expire         integer,
-    network                 text                not null,
-    group_full_path         text                not null,
-    group_uid               text                not null,
-    totp                    text
+    uid             TEXT                not null,
+    email           text                not null,
+    name            text                not null,
+    profilepic      text,
+    max_devices     integer default 999 not null,
+    password        text                not null,
+    password_expire integer,
+    network         text                not null,
+    group_full_path text                not null,
+    group_uid       text                not null,
+    totp            text                not null,
+    role            text                not null,
+    tags            text                not null
 );
 
