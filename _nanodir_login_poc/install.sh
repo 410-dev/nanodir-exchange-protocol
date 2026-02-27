c1cf04c484e4c4828f8e6998fc9fce11afa830ea
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-02-27 14:42:39 +0900

    Update install.sh

--- _nanodir_login_poc/install.sh
+++ _nanodir_login_poc/install.sh
@@ -12,14 +12,15 @@ ndauthhook="/usr/local/bin/ndauthhook.py"
 chmod +x "$ndauthhook"
 chown root:root "$ndauthhook"
 
-# Prepend the hook to /etc/pam.d/gdm-passwd
-pam_file="/etc/pam.d/gdm-passwd"
-hook_line="auth optional pam_exec.so ${ndauthhook}"
+# Prepend the hook to /etc/pam.d/gdm-password
+pam_file="/etc/pam.d/gdm-password"
+hook_line="auth optional pam_exec.so expose_authtok quiet ${ndauthhook}"
+
 
 if ! grep -Fxq "$hook_line" "$pam_file"; then
-    echo "$hook_line" | cat - "$pam_file" > temp && mv temp "$pam_file"
-    echo "Hook added to $pam_file"
+    echo "Adding authentication hook to $pam_file"
+    sed -i "1i $hook_line" "$pam_file"
 else
-    echo "Hook already present in $pam_file"
+    echo "Authentication hook already present in $pam_file"
 fi
 
